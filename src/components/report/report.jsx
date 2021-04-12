86609e76e0ca851aa0d0be3b4393c15ec7f52a5c
rana <zofaghari.r@dpi.ir>
2021-04-12 16:55:59 +0430

    report

--- src/components/report/report.jsx
+++ src/components/report/report.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import "../../styles/report.css"
+import "../../styles/report.css";
+import Box from "./box.json";
+import * as icon from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Report = () => {
     return (
@@ -7,6 +10,20 @@ const Report = () => {
             <text className="reportText">
                 Report
             </text>
+            {Box.map((item, index) => {
+                const iconName = icon[item.iconStyle];
+
+                return (
+                    <div className="box" key={index}>
+                        <span className="mapStyle">
+                            <FontAwesomeIcon icon={iconName} className="reportIcon" />
+                            <span className="titleStyle">
+                                {item.title}
+                            </span>
+                        </span>
+                    </div>
+                )
+            })}
         </div>
     )
 }
